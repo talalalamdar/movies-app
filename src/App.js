b9e78d89b95e93b89f358350902d141af8fd079b
talalalamdar <talalalamdar@gmail.com>
2020-04-24 11:44:15 +0200

    fixing upload issue

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import 'pure-react-carousel/dist/react-carousel.es.css';
 
 import cinema2 from './assets/tv_monitors.jpg'
 import MovieDBLogo from './assets/movie-db-header-logo.png'
+import MoviesVideo from './assets/movies-intro.mp4'
 
 // import icons, and tools for react router, redux adn store
 import { connect } from "react-redux"
@@ -16,9 +17,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="header">
-          <img className="header-img" src={cinema2} alt='header-img' />
+          <video className="header-img" autoPlay muted loop>
+            <source src={MoviesVideo} type="video/mp4" />
+          </video>
 
-          <div style={{ position: 'absolute', width: '100%', height: '100%', top: 0, right: 0, backgroundColor: 'rgba(59, 122, 40, 0.8)' }}>
+          <div style={{ position: 'absolute', width: '100%', height: '100%', top: 0, right: 0, backgroundColor: 'rgba(59, 122, 40, 0.3)' }}>
             <img style={{ position: 'absolute', top: 8, right: 8 }} src={MovieDBLogo} alt='movie-db-logo' width={'5%'} />
 
             <header className="App-header">
